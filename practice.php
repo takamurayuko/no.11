fe0681d9badf3350a74256cebe2a3946ad774b05
takamura <res_55_9@yahoo.co.jp>
2023-03-07 05:05:17 +0000

    no11  second commit

--- practice.php
+++ practice.php
@@ -4,3 +4,11 @@ echo 'hello php!';
 $a = 3;
 $b = 7;
 echo $a+$b;
+
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+echo $array_month[7];
+
+$hello = "Hello,";
+$name = "Yuko";
+$world = "s World!";
+echo $hello . $name . $world;

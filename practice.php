92fcc52a03187429adcba0c997f7891c8b0f1308
takamura <res_55_9@yahoo.co.jp>
2023-03-07 05:43:59 +0000

    no11 thaadcommit

--- practice.php
+++ practice.php
@@ -12,3 +12,26 @@ $hello = "Hello,";
 $name = "Yuko";
 $world = "s World!";
 echo $hello . $name . $world;
+
+$tech_boost = '「';
+$tech_boost .= 'tech';
+$tech_boost .=' boost」';
+echo $tech_boost;
+
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月",
+];
+echo $calendar_2018["December"];
+
+    
